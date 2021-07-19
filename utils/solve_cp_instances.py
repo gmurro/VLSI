2f27227f1f3b48d421941ae09ecb16b28673b694
giuseppeboezio <giuseppe.boezio.gb@gmail.com>
2021-07-19 22:47:08 +0200

    Dual modeling with cumulative constraints

--- utils/solve_cp_instances.py
+++ utils/solve_cp_instances.py
@@ -13,14 +13,18 @@ def main():
     parser.add_argument("-o", "--out_dir",
                         help="Path to the directory that will contain the output solutions in .txt format",
                         required=True, type=str)
+    parser.add_argument("-c", "--cores", help="Number of cores", required=False, type=int)
+    parser.add_argument("-s", "--stat", help="Output solver statistics", action='store_true')
     args = parser.parse_args()
 
     model = args.model
     in_dir = args.in_dir
     out_dir = args.out_dir
+    cores = args.cores if args.cores is not None else 1
 
     for in_file in glob(os.path.join(in_dir, '*.dzn')):
-        command = f'minizinc --solver Gecode -t 300000 {model} {in_file}'
+        command = f'minizinc --solver Gecode -p {cores} -t 300000 --solver-statistics {model} {in_file}' \
+            if args.stat else f'minizinc --solver Gecode -p {cores} -t 300000 {model} {in_file}'
         instance_name = in_file.split('/')[-1]
         instance_name = instance_name[:len(instance_name) - 4]
         out_file = os.path.join(out_dir, instance_name + '-out.txt')
