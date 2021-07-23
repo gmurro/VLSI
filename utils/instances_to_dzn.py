914d71c6fd4c8ea456abbeb6bc61b12d53c73495
gmurro <g.murro23@gmail.com>
2021-07-23 16:29:12 +0200

    add order of magnitude w

--- utils/instances_to_dzn.py
+++ utils/instances_to_dzn.py
@@ -3,38 +3,43 @@ import sys
 txt_path = "../data/instances_txt"
 dzn_path = "../data/instances_dzn"
 
-if len(sys.argv) != 3:
-    print('Usage: instances_to_dzn.py <input_file> <output_file>')
-    sys.exit(1)
+#if len(sys.argv) != 3:
+    #print('Usage: instances_to_dzn.py <input_file> <output_file>')
+    #sys.exit(1)
 
 # if you want to do all the files decomment and indent correctly
-# for k in range(1, 41):
-# output_filename = dzn_path + "/ins-" + str(k) + ".dzn"
-# input_filename = txt_path + "/ins-" + str(k) + ".txt"
+for k in range(1, 41):
+    output_filename = dzn_path + "/ins-" + str(k) + ".dzn"
+    input_filename = txt_path + "/ins-" + str(k) + ".txt"
 
-input_filename = txt_path + "/" + sys.argv[1]
-output_filename = dzn_path + "/" + sys.argv[2]
+#input_filename = txt_path + "/" + sys.argv[1]
+#output_filename = dzn_path + "/" + sys.argv[2]
 
-with open(input_filename, 'r') as f_in:
-    lines = f_in.read().splitlines()
+    with open(input_filename, 'r') as f_in:
+        lines = f_in.read().splitlines()
 
-    w = lines[0]
-    n = lines[1]
+        w = lines[0]
+        n = lines[1]
 
-    x = []
-    y = []
+        x = []
+        y = []
 
-    for i in range(int(n)):
-        split = lines[i + 2].split(' ')
-        x.append(int(split[0]))
-        y.append(int(split[1]))
+        for i in range(int(n)):
+            split = lines[i + 2].split(' ')
+            x.append(int(split[0]))
+            y.append(int(split[1]))
 
-    l_max = sum(y)
+        l_max = sum(y)
 
-    with open(output_filename, 'w+') as f_out:
-        f_out.write('w = {};\n'.format(w))
-        f_out.write('n = {};\n'.format(n))
+        # compute order of magnitude of w
+        len_w = len(str(w))
+        mag_w = 10**len_w
 
-        f_out.write('x = {};\n'.format(x))
-        f_out.write('y = {};\n'.format(y))
-        f_out.write('l_max={};\n'.format(l_max))
+        with open(output_filename, 'w+') as f_out:
+            f_out.write('w = {};\n'.format(w))
+            f_out.write('n = {};\n'.format(n))
+
+            f_out.write('x = {};\n'.format(x))
+            f_out.write('y = {};\n'.format(y))
+            f_out.write('l_max={};\n'.format(l_max))
+            f_out.write('mag_w={};\n'.format(mag_w))
