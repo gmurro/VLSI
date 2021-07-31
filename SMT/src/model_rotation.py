3c1a873da772b51c2899143203ba27ff430c9279
giuseppeboezio <giuseppe.boezio.gb@gmail.com>
2021-07-31 18:45:42 +0200

    Final implementation of rotation with SMT

--- SMT/src/model_rotation.py
+++ SMT/src/model_rotation.py
@@ -19,12 +19,27 @@ def solve_instance(w, n, x, y, l_max, mag_w):
     p_x = [Int("p_x_%s" % str(i+1)) for i in range(n)]
     p_y = [Int("p_y_%s" % str(i+1)) for i in range(n)]
 
+    # rotation array
+    rotation = [Bool("rot_%s" % str(i+1)) for i in range(n)]
+
+    # real dimensions of points considering rotation
+    x_r = [Int("x_r_%s" % str(i+1)) for i in range(n)]
+    y_r = [Int("y_r_%s" % str(i+1)) for i in range(n)]
+
     # maximum height to minimize
     length = Int("length")
 
     # domain bounds
-    domain_x = [And(p_x[i] >= 0,p_x[i] <= w-min(x)) for i in range(n)]
-    domain_y = [And(p_y[i] >= 0,p_y[i] <= l_max-min(y)) for i in range(n)]
+    domain_x = [And(p_x[i] >= 0, p_x[i] <= w-min(x)) for i in range(n)]
+    domain_y = [And(p_y[i] >= 0, p_y[i] <= l_max-min(y)) for i in range(n)]
+
+    # lengths bound
+    width_bound = [And(x_r[i] >= 1, x_r[i] <= w) for i in range(n)]
+    heigth_bound = [And(y_r[i] >= 1, y_r[i] <= l_max) for i in range(n)]
+
+    # relationship between rotation and dimensions
+    rotation_rel = [If(rotation[i], And(x_r[i] == y[i], y_r[i] == x[i]), And(x_r[i] == x[i], y_r[i] == y[i]))
+                    for i in range(n)]
 
     # different coordinates
     all_different = [Distinct([mag_w * p_x[i] + p_y[i]]) for i in range(n)]
@@ -67,7 +82,7 @@ def solve_instance(w, n, x, y, l_max, mag_w):
     # setting the optimizer
     opt = Optimize()
     opt.add(domain_x + domain_y + overlapping + all_different + objective + cumulative_x +
-            cumulative_y + max_w + max_h + symmetry + left)
+            cumulative_y + max_w + max_h + symmetry + left + width_bound + heigth_bound + rotation_rel)
     opt.minimize(length)
 
     # maximum time of execution
@@ -76,6 +91,7 @@ def solve_instance(w, n, x, y, l_max, mag_w):
 
     p_x_sol = []
     p_y_sol = []
+    rot_sol = []
     length_sol = ""
     # solving the problem
     if opt.check() == sat:
@@ -84,6 +100,7 @@ def solve_instance(w, n, x, y, l_max, mag_w):
         for i in range(n):
             p_x_sol.append(model.evaluate(p_x[i]).as_string())
             p_y_sol.append(model.evaluate(p_y[i]).as_string())
+            rot_sol.append(str(model.evaluate(rotation[i])))
         length_sol = model.evaluate(length).as_string()
     elif opt.reason_unknown() == "timeout":
         msg = "Solver timeout"
