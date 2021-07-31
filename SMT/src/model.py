First correct implementation of SMT (without rotation)

SMT/src/model.py
@@ -1,10 +1,18 @@
 import sys
 from z3 import *
-from numpy import argmax
+import numpy as np
 from itertools import combinations
+import time
 
 
-def cumulative(start, duration, resources, total):
+def z3_max(vector):
+    maximum = vector[0]
+    for value in vector[1:]:
+        maximum = If(value > maximum, value, maximum)
+    return maximum
+
+
+def z3_cumulative(start, duration, resources, total):
 
     decomposition = []
     for u in resources:
@@ -18,7 +26,7 @@ def cumulative(start, duration, resources, total):
 def solve_instance(w, n, x, y, l_max, mag_w):
 
     # index of the circuit with the highest value
-    index = argmax(y)
+    index = np.argmax(np.asarray(y))
 
     # definition of the variables
 
@@ -37,19 +45,23 @@ def solve_instance(w, n, x, y, l_max, mag_w):
     all_different = [Distinct([mag_w * p_x[i] + p_y[i]]) for i in range(n)]
 
     # value of l
-    objective = length == max([p_y[i] + y[i] for i in range(n)])
+    objective = [length == z3_max([p_y[i] + y[i] for i in range(n)])]
 
     # cumulative constraints
-    cumulative_y = cumulative(p_y, y, x, w)
-    cumulative_x = cumulative(p_x, x, y, l_max)
+    cumulative_y = z3_cumulative(p_y, y, x, w)
+    cumulative_x = z3_cumulative(p_x, x, y, l_max)
 
     # maximum width
-    max_w = [max([p_x[i] + x[i] for i in range(n)]) <= w]
+    max_w = [z3_max([p_x[i] + x[i] for i in range(n)]) <= w]
 
     # maximum height
-    max_h = [max([p_y[i] + y[i] for i in range(n)]) <= l_max]
+    max_h = [z3_max([p_y[i] + y[i] for i in range(n)]) <= l_max]
 
     # relationship avoiding overlapping
+    '''overlapping = [Or(p_x[i] + x[i] <= p_x[j],
+                              p_x[j] + x[j] <= p_x[i],
+                              p_y[i] + y[i] <= p_y[j],
+                              p_y[j] + y[j] <= p_y[i]) for j in range(n) for i in range(j)]'''
     overlapping = []
     for (i,j) in combinations(range(n),2):
         overlapping.append(Or(p_x[i] + x[i] <= p_x[j],
@@ -65,7 +77,7 @@ def solve_instance(w, n, x, y, l_max, mag_w):
 
     # setting the optimizer
     opt = Optimize()
-    opt.add(domain_x + domain_y + all_different + [objective] + cumulative_x +
+    opt.add(domain_x + domain_y + overlapping + all_different + objective + cumulative_x +
             cumulative_y + max_w + max_h + symmetry)
     opt.minimize(length)
 
@@ -76,6 +88,7 @@ def solve_instance(w, n, x, y, l_max, mag_w):
     # solving the problem
     if opt.check() == sat:
         model = opt.model()
+        print(model)
         print(int(model.evaluate(length).as_string()))
     elif opt.reason_unknown() == "timeout":
         print("Solver timeout")
@@ -84,14 +97,16 @@ def solve_instance(w, n, x, y, l_max, mag_w):
 
 
 def main():
-    w = 11
-    n = 7
-    x = [3, 3, 3, 3, 5, 5, 8]
-    y = [3, 4, 5, 6, 3, 4, 4]
-    l_max = 29
+    w = 31
+    n = 19
+    x = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 12]
+    y = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 22, 31, 3, 7, 8, 13, 31]
+    l_max = 217
     mag_w = 100
-
+    start_time = time.time()
     solve_instance(w, n, x, y, l_max, mag_w)
+    end_time = time.time() - start_time
+    print(end_time)
 
 
 main()