d0a2de51b9f3d45e94ffe0e058d340fc98c78252
giuseppeboezio <giuseppe.boezio.gb@gmail.com>
2021-08-01 14:22:48 +0200

    Final implementation of the script for executing all instances on SMT model both with and without rotation of circuits

--- SMT/src/solve_smt_instances.py
+++ SMT/src/solve_smt_instances.py
@@ -14,24 +14,24 @@ def main():
                         help="Path to the directory that will contain the output solutions in .txt format",
                         required=True, type=str)
     parser.add_argument("-r","--rotation", help="Flag to decide whether it is possible use rotated circuits",
-                        required=False, type=bool)
+                        required=False, action='store_true')
     args = parser.parse_args()
 
     in_dir = args.in_dir
     out_dir = args.out_dir
-    rotation = args.rotation if args.rotation is not None else 0
+    rotation = args.rotation if args.rotation is not None else False
 
-    if rotation == 0:
-
-        # rotation model has been enabled
-        for in_file in glob(os.path.join(in_dir, '*.txt')):
-            mdr.solve_instance(in_file, out_dir)
-
-    else:
+    if not rotation:
 
         # running standard model
         for in_file in glob(os.path.join(in_dir, '*.txt')):
             md.solve_instance(in_file, out_dir)
+
+    else:
+
+        # running rotation model
+        for in_file in glob(os.path.join(in_dir, '*.txt')):
+            mdr.solve_instance(in_file, out_dir)
 
 
 if __name__ == '__main__':
