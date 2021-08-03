b3291ca979eab46aff5db9f90368d83f8c5bd918
gmurro <g.murro23@gmail.com>
2021-08-03 18:40:59 +0200

    test sat but does not work

---
+++ SAT/src/model_2.py
@@ -0,0 +1,273 @@
+from z3 import *
+import numpy as np
+from itertools import combinations
+import time
+import matplotlib.pyplot as plt
+
+def read_file(input_filename):
+
+    with open(input_filename, 'r') as f_in:
+
+        lines = f_in.read().splitlines()
+
+        w = lines[0]
+        n = lines[1]
+
+        x = []
+        y = []
+
+        for i in range(int(n)):
+            split = lines[i + 2].split(' ')
+            x.append(int(split[0]))
+            y.append(int(split[1]))
+
+        l_max = sum(y)
+
+        # compute order of magnitude of w
+        len_w = len(str(w))
+        mag_w = 10 ** len_w
+
+        return int(w), int(n), x, y, l_max, mag_w
+
+
+def write_file(w, n, x, y, p_x_sol, p_y_sol, length, out_file):
+
+    with open(out_file, 'w+') as f_out:
+
+        f_out.write('{} {}\n'.format(w, length))
+        f_out.write('{}\n'.format(n))
+
+        for i in range(n):
+            f_out.write('{} {} {} {}\n'.format(x[i], y[i], p_x_sol[i], p_y_sol[i]))
+
+
+def z3_max(vector):
+    maximum = vector[0]
+    for value in vector[1:]:
+        maximum = If(value > maximum, value, maximum)
+    return maximum
+
+
+def at_least_one(bool_vars):
+    return Or(bool_vars)
+
+
+def at_most_one(bool_vars):
+    return [Not(And(pair[0], pair[1])) for pair in combinations(bool_vars, 2)]
+
+
+def exactly_one(bool_vars):
+    return at_most_one(bool_vars) + [at_least_one(bool_vars)]
+
+
+def solve_instance(in_file, out_dir):
+
+    instance_name = in_file.split('\\')[-1] if os.name == 'nt' else in_file.split('/')[-1]
+    instance_name = instance_name[:len(instance_name) - 4]
+    out_file = os.path.join(out_dir, instance_name + '-out.txt')
+
+    w, n, x, y, l_max, mag_w = read_file(in_file)
+
+    # Define solver and base model
+    solver = Solver()
+
+    # definition of the variables
+
+    # coordinates of the points
+    plate = [[[Bool(f"b_{i}_{j}_{k}") for k in range(n)] for j in range(w)] for i in range(l_max)]
+
+    # maximum height to minimize
+    l = [Bool(f"l_{i}") for i in range(l_max)]
+
+    print('Adding constraints...')
+
+    # Each cell in the plate has only one value
+    [solver.add(at_most_one(plate[i][j])) for i in range(l_max) for j in range(w)]
+
+    # Iterate over all the n circuits
+    for k in range(n):
+        x_k = x[k]
+        y_k = y[k]
+
+        # clause containing all possible positions of each circuit into the plate
+        all_circuit_positions = []
+
+        # Iterate over all the coordinates where p can fit
+        for i in range(l_max - y_k + 1):
+            for j in range(w - x_k + 1):
+
+                # all cells corresponding to the circuit position
+                circuit_positioning = []
+
+                # Iterate over the cells of circuit's patch
+                for oy in range(i, i+y_k):
+                    for ox in range(j, j+x_k):
+                        circuit_positioning.append(plate[oy][ox][k])
+
+                all_circuit_positions.append(And(circuit_positioning))
+
+    # Exactly one
+    solver.add(at_least_one(all_circuit_positions))
+    solver.add(at_most_one(all_circuit_positions))
+
+    # compute the length
+    #solver.add([l[i] == And( [Or(list(np.concatenate(plate[i]).flat) )] + [Not(Or( list(np.concatenate(plate[j]).flat))) for j in range(i+1, l_max)]) for i in range(l_max)])
+
+    # value of length
+    #objective = [length == z3_max([k + y[i] if p_y[i][k] is True else None for k in range(l_max - min(y)) for i in range(n)])]
+
+
+    # maximum time of execution
+    timeout = 300000
+    solver.set(timeout=timeout)
+
+    p_x_sol = []
+    p_y_sol = []
+
+    # solving the problem
+
+    print(f'{out_file}:', end='\t', flush=True)
+    start_time = time.time()
+
+    if solver.check() == sat:
+        model = solver.model()
+        elapsed_time = time.time() - start_time
+        print(f'{elapsed_time * 1000:.1f} ms')
+        """
+        sol = []
+        for i in range(l_max):
+            for j in range(w):
+                for k in range(n):
+                    if model.evaluate(plate[i][j][k]):
+                        sol[i].append(k + 1)"""
+
+
+        for k in range(n):
+            sol = []
+            for i in range(l_max):
+                sol.append([])
+                for j in range(w):
+                    if model.evaluate(plate[i][j][k]):
+                        sol[i].append(1)
+                    else:
+                        sol[i].append(0)
+
+            plt.imshow(sol)
+            plt.show()
+
+
+
+        # getting values of variables
+        #length_sol = model.evaluate(length).as_string()
+
+        # storing result
+        #write_file(w, n, x, y, p_x_sol, p_y_sol, length_sol, out_file)
+
+    elif solver.reason_unknown() == "timeout":
+        elapsed_time = time.time() - start_time
+        print(f'{elapsed_time * 1000:.1f} ms')
+        print("Timeout")
+    else:
+        elapsed_time = time.time() - start_time
+        print(f'{elapsed_time * 1000:.1f} ms')
+        print("Unsatisfiable")
+
+
+
+    """
+    # index of the circuit with the highest value
+    index = np.argmax(np.asarray(y))
+
+    # coordinates of the points
+    p_x = [Int("p_x_%s" % str(i+1)) for i in range(n)]
+    p_y = [Int("p_y_%s" % str(i+1)) for i in range(n)]
+
+    # maximum height to minimize
+    length = Int("length")
+
+    # domain bounds
+    domain_x = [And(p_x[i] >= 0,p_x[i] <= w-min(x)) for i in range(n)]
+    domain_y = [And(p_y[i] >= 0,p_y[i] <= l_max-min(y)) for i in range(n)]
+
+    # different coordinates
+    all_different = [Distinct([mag_w * p_x[i] + p_y[i]]) for i in range(n)]
+
+    # value of l
+    objective = [length == z3_max([p_y[i] + y[i] for i in range(n)])]
+
+    # cumulative constraints
+    cumulative_y = z3_cumulative(p_y, y, x, w)
+    cumulative_x = z3_cumulative(p_x, x, y, l_max)
+
+    # maximum width
+    max_w = [z3_max([p_x[i] + x[i] for i in range(n)]) <= w]
+
+    # maximum height
+    max_h = [z3_max([p_y[i] + y[i] for i in range(n)]) <= l_max]
+
+    # relationship avoiding overlapping
+    '''overlapping = [Or(p_x[i] + x[i] <= p_x[j],
+                              p_x[j] + x[j] <= p_x[i],
+                              p_y[i] + y[i] <= p_y[j],
+                              p_y[j] + y[j] <= p_y[i]) for j in range(n) for i in range(j)]'''
+    overlapping = []
+    for (i,j) in combinations(range(n),2):
+        overlapping.append(Or(p_x[i] + x[i] <= p_x[j],
+                              p_x[j] + x[j] <= p_x[i],
+                              p_y[i] + y[i] <= p_y[j],
+                              p_y[j] + y[j] <= p_y[i])
+                           )
+
+    # the circuit whose height is the maximum among all circuits is put in the left-bottom corner
+    symmetry = [And(p_x[index] == 0, p_y[index] == 0)]
+
+    # setting the optimizer
+    opt = Optimize()
+    opt.add(domain_x + domain_y + overlapping + all_different + objective + cumulative_x +
+            cumulative_y + max_w + max_h + symmetry)
+    opt.minimize(length)
+
+    # maximum time of execution
+    timeout = 300000
+    opt.set(timeout=timeout)
+
+    p_x_sol = []
+    p_y_sol = []
+
+    # solving the problem
+
+    print(f'{out_file}:', end='\t', flush=True)
+    start_time = time.time()
+
+    if opt.check() == sat:
+        model = opt.model()
+        elapsed_time = time.time() - start_time
+        print(f'{elapsed_time * 1000:.1f} ms')
+        # getting values of variables
+        for i in range(n):
+            p_x_sol.append(model.evaluate(p_x[i]).as_string())
+            p_y_sol.append(model.evaluate(p_y[i]).as_string())
+        length_sol = model.evaluate(length).as_string()
+
+        # storing result
+        write_file(w, n, x, y, p_x_sol, p_y_sol, length_sol, out_file)
+
+    elif opt.reason_unknown() == "timeout":
+        elapsed_time = time.time() - start_time
+        print(f'{elapsed_time * 1000:.1f} ms')
+        print("Timeout")
+    else:
+        elapsed_time = time.time() - start_time
+        print(f'{elapsed_time * 1000:.1f} ms')
+        print("Unsatisfiable")
+    """
+
+def main():
+
+    in_file = "..\..\data\instances_txt\ins-1.txt"
+    out_dir = "out"
+    solve_instance(in_file, out_dir)
+
+
+if __name__ == '__main__':
+    main()
---
+++ SAT/src/solve_sat_instances.py
@@ -0,0 +1,41 @@
+import argparse
+from glob import glob
+import model as md
+from z3 import *
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--in_dir", help="Path to the directory containing the initial instances",
+                        required=True, type=str)
+    parser.add_argument("-o", "--out_dir",
+                        help="Path to the directory that will contain the output solutions in .txt format",
+                        required=True, type=str)
+    parser.add_argument("-r","--rotation", help="Flag to decide whether it is possible use rotated circuits",
+                        required=False, action='store_true')
+    args = parser.parse_args()
+
+    in_dir = args.in_dir
+    out_dir = args.out_dir
+    rotation = args.rotation if args.rotation is not None else False
+
+    if not rotation:
+
+        # running standard model
+        for in_file in glob(os.path.join(in_dir, '*.txt')):
+            md.solve_instance(in_file, out_dir)
+
+    '''else:
+
+        # running rotation model
+        for in_file in glob(os.path.join(in_dir, '*.txt')):
+            mdr.solve_instance(in_file, out_dir)'''
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
