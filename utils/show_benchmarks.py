0043673465b4483ed903c6b33498048e8d48539a
giuseppeboezio <giuseppe.boezio.gb@gmail.com>
2021-08-09 22:23:22 +0200

    First implementation of the script to show the histogram

--- utils/show_benchmarks.py
+++ utils/show_benchmarks.py
@@ -1,8 +1,10 @@
 import os
 import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
 
 
-def get_dataframe(list_of_dir):
+def get_dataframe(list_of_dir, column_names):
 
     dataframe_list = []
     # for each instance in the folder create a dictionary which associates to each instance the elapsed time
@@ -12,19 +14,29 @@ def get_dataframe(list_of_dir):
             file_ins = os.path.join(directory, instance_name)
             with open(file_ins, 'r') as f_in:
                 lines = f_in.read().splitlines()
-                elapsed_time = lines[-1]
-                dir_dict[instance_name] = elapsed_time
+                elapsed_time = float(lines[-1])
+                dir_dict["{}".format(instance_name[:-4])] = elapsed_time
         dataframe_list.append(dir_dict)
 
     # creation of the datafame to use for visulizaion
-    df = pd.DataFrame(dataframe_list)
+    df = pd.DataFrame(dataframe_list, columns=column_names)
+
+    # setting NaN to 0 (instance too hard to execute)
+    df.fillna(0)
+
     return df
 
 
+def show_histogram(df):
+
+    df.plot.bar()
+
+
 def main():
+    column_names = ['CP', 'SAT', 'SMT']
     directories = ["../CP/out", "../SAT/src/out", "../SMT/out"]
-    data = get_dataframe(directories)
-    print(data)
+    data = get_dataframe(directories, column_names)
+    show_histogram(data)
 
 
 if __name__ == "__main__":
