313c45dac58b2e648159b76386049b86dbc46c81
giuseppeboezio <giuseppe.boezio.gb@gmail.com>
2021-08-13 11:19:01 +0200

    changing name in the output

--- CP/src/solve_instance.py
+++ CP/src/solve_instance.py
@@ -10,7 +10,7 @@ def solve_instance(cores, model, in_file, out_dir):
 
     instance_name = in_file.split('\\')[-1] if os.name == 'nt' else in_file.split('/')[-1]
     instance_name = instance_name[:len(instance_name) - 4]
-    out_file = os.path.join(out_dir, instance_name + '-out_final.txt')
+    out_file = os.path.join(out_dir, instance_name + '-out.txt')
     with open(out_file, 'w') as f:
         print(f'{out_file}:', end='\t', flush=True)
         start_time = time()
